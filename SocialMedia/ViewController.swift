89ac2815f81a3a09678f4a06fa3880ec18379b32
Uncle4est <hellos.fibroid.0a@icloud.com>
2022-10-02 20:01:39 +0300

    Initial Commit

---
+++ SocialMedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialMedia
+//
+//  Created by Nicholas Smith on 10/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
